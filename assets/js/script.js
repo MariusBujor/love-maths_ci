661c2edbfc6166cfb5a73366155f7b5ce1030ac7
Marius Corneliu Bujor <bujormarius1982@gmail.com>
2021-09-29 16:13:24 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+
+function calculateCorectAnswer(){
+    
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAdditionQuestion(){
+
+}
+function displaySubtractQuestion(){
+
+}
+function displayMultiplyQuestion(){
+
+}

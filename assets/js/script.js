979dc8bafc94a48267d941204c557206b9cc219c
Marius Corneliu Bujor <bujormarius1982@gmail.com>
2021-10-03 19:46:38 +0000

    Add division section / chhallanged completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,7 +43,15 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1,num2);
     } else if (gameType === "subtract" ){
         displaySubtractQuestion(num1,num2);
-    }else {
+    }else if  (gameType === "division" ){
+        
+    let numA = Math.floor(Math.random()*5)+1;
+    let numB = Math.floor(Math.random()*5)+1;
+    let num1 = numA * numB;
+    let num2 = numB;
+    displayDivisionQuestion(num1, num2);
+
+    } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -91,7 +99,11 @@ function calculateCorrectAnswer() {
     }else if (operator === "-"){
         return [operand1 - operand2, "subtract"];
 
+    } else if (operator === "/"){
+        return [operand1 / operand2, "division"];
+
     } else {
+
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -142,4 +154,11 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "x";
     
     
+}
+
+function displayDivisionQuestion(operand1,operand2) {
+
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
 }
